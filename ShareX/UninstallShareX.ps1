d0f9e3133d80944948bec8d6165e844e20a3fb44
Daniel Williams <96486477+strongestgeek@users.noreply.github.com>
2025-09-29 17:35:44 +0100

    Update UninstallShareX.ps1

--- ShareX/UninstallShareX.ps1
+++ ShareX/UninstallShareX.ps1
@@ -3,17 +3,29 @@
     Uninstalls ShareX application silently.
 .DESCRIPTION
     This script attempts to find and run the ShareX uninstaller in silent mode.
-    It checks for the uninstaller in the default installation location and provides feedback on the process.
+    It checks for the uninstaller in the default installation locations and provides feedback on the process.
 .NOTES
-    Version: 1.0
+    Version: 1.1
     Author: strongestgeek
 #>
 
-$uninstaller = "$env:LOCALAPPDATA\Programs\ShareX\unins000.exe"
-if (Test-Path $uninstaller) {
-    Write-Host "Uninstaller found: $uninstaller"
-    Start-Process -FilePath $uninstaller -ArgumentList '/VERYSILENT', '/NORESTART' -WindowStyle Hidden
-    Write-Host "Uninstalled ShareX"
-} else {
-    Write-Host "Uninstaller not found"
+$uninstallerLocations = @(
+    "$env:LOCALAPPDATA\Programs\ShareX\unins000.exe",
+    "C:\Program Files\ShareX\unins000.exe"
+)
+
+$uninstallerFound = $false
+
+foreach ($uninstaller in $uninstallerLocations) {
+    if (Test-Path $uninstaller) {
+        Write-Host "Uninstaller found: $uninstaller"
+        Start-Process -FilePath $uninstaller -ArgumentList '/VERYSILENT', '/NORESTART' -WindowStyle Hidden
+        Write-Host "Uninstalled ShareX"
+        $uninstallerFound = $true
+        break
+    }
+}
+
+if (-not $uninstallerFound) {
+    Write-Host "Uninstaller not found in any of the expected locations"
 }
