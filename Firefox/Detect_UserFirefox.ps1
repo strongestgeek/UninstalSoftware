f419be8d9c2cd013f3b64b1d4992d8e7063db977
Daniel Williams <96486477+strongestgeek@users.noreply.github.com>
2025-11-13 15:33:35 +0000

    Add files via upload

---
+++ Firefox/Detect_UserFirefox.ps1
@@ -0,0 +1,20 @@
+# Function to detect and uninstall Firefox for each user profile
+function Get-UserFirefox {
+    # Get all user profile directories from C:\Users
+    $userProfiles = Get-ChildItem -Path "C:\Users" -Directory
+    foreach ($userProfile in $userProfiles) {
+        $userFoxPath = Join-Path -Path $userProfile.FullName -ChildPath "AppData\Local\Mozilla Firefox"
+        $FoxExecutable = Join-Path -Path $userFoxPath -ChildPath "firefox.exe"
+        # Check if firefox.exe exists, indicating it is installed in this user's profile
+        if (Test-Path -Path $FoxExecutable) {
+            # Look for any uninstaller file"
+            Write-Host "Local User Firefox found."
+            Exit 0
+        } else {
+            Write-Host "Local User Firefox not found."
+            Exit 1
+        }
+    }
+}
+
+Get-UserFirefox 
