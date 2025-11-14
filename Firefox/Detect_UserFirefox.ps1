b8e4bffabfd82bdb3e8cbd5a6724b483947ffba1
Daniel Williams <96486477+strongestgeek@users.noreply.github.com>
2025-11-14 10:13:48 +0000

    Update Detect_UserFirefox.ps1

--- Firefox/Detect_UserFirefox.ps1
+++ Firefox/Detect_UserFirefox.ps1
@@ -1,22 +1,18 @@
-# Function to detect Firefox for each user profile
-function Get-UserFirefox {
-    # Get all user profile directories from C:\Users
-    $userProfiles = Get-ChildItem -Path "C:\Users" -Directory
-    foreach ($userProfile in $userProfiles) {
-        $userFoxPath = Join-Path -Path $userProfile.FullName -ChildPath "AppData\Local\Mozilla Firefox"
-        $FoxExecutable = Join-Path -Path $userFoxPath -ChildPath "firefox.exe"
-        # Check if firefox.exe exists, indicating it is installed in this user's profile
-        if (Test-Path -Path $FoxExecutable) {
-            # Look for any uninstaller file"
-            Write-Host "Local User Firefox found."
-            Exit 1
-        } else {
-            Write-Host "Local User Firefox not found."
-            Exit 0
-        }
-    }
-}
-
-
-Get-UserFirefox 
-
+# Get all user profile directories from C:\Users
+$userProfiles = Get-ChildItem -Path "C:\Users" -Directory
+foreach ($userProfile in $userProfiles) {
+	$userFoxPath = Join-Path -Path $userProfile.FullName -ChildPath "AppData\Local\Mozilla Firefox"
+	$FoxExecutable = Join-Path -Path $userFoxPath -ChildPath "firefox.exe"
+	# Check if firefox.exe exists, indicating it is installed in this user's profile
+	if (Test-Path -Path $FoxExecutable) {
+		# Look for any uninstaller file"
+		Write-Host $FoxExecutable
+		Write-Host "Local User Firefox found."
+		Write-Host "Exit 1."
+		Exit 1
+	} else {
+		Write-Host "Local User Firefox not found."
+		Write-Host "Exit 0."
+		Exit 0
+	}
+} 
